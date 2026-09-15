d296d2f476849fcb4892b0229e956de1f246c6fc
themagiche <magiche.mc@gmail.com>
2026-09-16 01:27:01 +0300

    removed mobile support

--- mobile/src/services/embedding/index.ts
+++
@@ -1,9 +0,0 @@
-export { warmupEmbedder, switchEmbeddingModel, embedText, embedBatch, isEmbedderReady, getEmbedDims, getCurrentModelId, getLastInitError } from './embedder';
-export type { DownloadProgress } from './embedder';
-export { searchVectors, semanticSearch, semanticSearchScored, cosineSimilarity, mmrSelect } from './vectorSearch';
-export type { SearchHit } from './vectorSearch';
-export { runBackfill, getBackfillCursor, backfillScenes, backfillNPCs, rebuildAllEmbeddings, runFullReindex } from './backfillRunner';
-export type { BackfillProgress } from './backfillRunner';
-export { enqueueProgressive, enqueueProgressiveWithExistingCheck, abortForCampaignSwitch, abortForModelSwitch, getQueueStats } from './embeddingScheduler';
-export type { ProgressiveChunk } from './embeddingScheduler';
-export { getForegroundPoolSize, poolEmbed, terminatePool, getActivePoolSize } from './embedderPool';
--- mobile/src/services/infrastructure/saveFilePicker.ts
+++
@@ -1,7 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-
-interface SaveFilePlugin {
-    copyToDownloads(options: { uri: string; filename: string }): Promise<void>;
-}
-
-export const SaveFile = registerPlugin<SaveFilePlugin>('SaveFile');
